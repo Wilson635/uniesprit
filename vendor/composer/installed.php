bb7b3048d71457c81f82be0bc0d997e07bec2a53
Wilson635 <ngahemeniw@gmail.com>
2025-03-18 09:32:42 -0400

    Mise à jour des fichiers et optimisation de l'exportation des fichiers

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '95515eced7cab9fe13be22bff8f41ef29538655f',
+        'reference' => '74d66febfa96aed70e9c257c3dfb93c28e402d8f',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '95515eced7cab9fe13be22bff8f41ef29538655f',
+            'reference' => '74d66febfa96aed70e9c257c3dfb93c28e402d8f',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
